7f973ec9a19dc2a7f6f2b2519ad1be74491b8338
iuzi <abneriuzirodriguesqueiroz@gmail.com>
2025-05-19 04:07:43 -0300

    release

--- practicing/ex009.py
+++ practicing/ex009.py
@@ -1,25 +1,14 @@
 n = int(input('Digite um número: '))
-a = n*0
-b = n*1
-c = n*2
-d = n*3
-e = n*4
-f = n*5
-g = n*6
-h = n*7
-i = n*8
-j = n*9
-k = n*10
 
-print('{}x0 = {}'.format(n, a))
-print('{}x1 = {}'.format(n, b))
-print('{}x2 = {}'.format(n, c))
-print('{}x3 = {}'.format(n, d))
-print('{}x4 = {}'.format(n, e))
-print('{}x5 = {}'.format(n, f))
-print('{}x6 = {}'.format(n, g))
-print('{}x7 = {}'.format(n, h))
-print('{}x8 = {}'.format(n, i))
-print('{}x9 = {}'.format(n, j))
-print('{}x10 = {}'.format(n, k))
-print('-------------------')
+print('-' * 12)
+print('{} x {:2} = {}'.format(n, 1, n*1))
+print('{} x {:2} = {}'.format(n, 2, n*2))
+print('{} x {:2} = {}'.format(n, 3, n*3))
+print('{} x {:2} = {}'.format(n, 4, n*4))
+print('{} x {:2} = {}'.format(n, 5, n*5))
+print('{} x {:2} = {}'.format(n, 6, n*6))
+print('{} x {:2} = {}'.format(n, 7, n*7))
+print('{} x {:2} = {}'.format(n, 8, n*8))
+print('{} x {:2} = {}'.format(n, 9, n*9))
+print('{} x {:2} = {}'.format(n, 10, n*10))
+print('-' * 12)
